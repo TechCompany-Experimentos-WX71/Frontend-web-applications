534ebac94c92c843e20306b2215addd7b502d791
Renato <u201511892@upc.edu.pe>
2023-04-22 20:44:38 -0500

    feat: navbar client and driver

--- src/app/client/pages/notifications/notifications.component.ts
+++ src/app/client/pages/notifications/notifications.component.ts
@@ -1,24 +1,28 @@
-
-import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.css']
+  styleUrls: ['./notifications.component.css'],
 })
-export class NotificationsComponent implements OnInit{
-  @Input() title='';
-  
-  
-  
-  public show=false;
-  
+export class NotificationsComponent implements OnInit {
+  @Input() title = '';
+
+  public show = false;
+
   ngOnInit(): void {}
- 
-  showModal(){
-    this.show=!this.show;
+
+  showModal() {
+    this.show = true;
   }
-  hideModal(){
-    this.show=false;
+  hideModal() {
+    this.show = false;
   }
 }
